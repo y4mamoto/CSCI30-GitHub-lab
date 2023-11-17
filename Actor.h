068bc5e0027272bd2be7355c546e0dac0441386c
jqTruong24 <jimmytruongq@gmail.com>
2023-11-16 21:54:56 -0800

    Class declaration, still need additional function and variable declaration

--- Actor.h
+++ Actor.h
@@ -3,6 +3,59 @@
 
 #include "GraphObject.h"
 
+using namespace std;
+
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+class Actor : public GraphObject
+{
+};
+
+class Person : public Actor
+{
+};
+class object : public Actor
+{
+};
+
+class Protester : public Person
+{
+};
+
+class Iceman : public Person
+{
+};
+
+class regularProtester : public Protester
+{
+};
+
+class hardcoreProtester : public Protester
+{
+};
+
+class waterRefill : public object
+{
+};
+
+class oil : public object
+{
+};
+
+class gold : public object
+{
+};
+
+class sonar : public object
+{
+};
+
+class Ice : public Actor
+{
+};
+
+class Boulder : public Actor
+{
+};
 
 #endif // ACTOR_H_
